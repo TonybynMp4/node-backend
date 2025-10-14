d676cbc6fabf92183f90d072001ae83a776095df
Tony <97451137+TonybynMp4@users.noreply.github.com>
2025-10-14 11:54:42 +0200

    feat: html route (exo 6)

--- express_server.js
+++ express_server.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.send('Hello from Express!');
 });
 
+app.get('/some-html', (req, res) => {
+  res.send('<html><body><h1>bonjour html</h1></body></html>');
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on http://localhost:${PORT}`);
 });
