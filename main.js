00c1245b855a9215009cd231d91c39bb0ee38261
Tony <97451137+TonybynMp4@users.noreply.github.com>
2025-10-15 11:44:39 +0200

    feat: middleware & static files

--- main.js
+++ main.js
@@ -5,7 +5,20 @@ import { router } from './src/routes.js';
 const app = express();
 const PORT = 3000;
 
+function loggerMiddleware(text) {
+	return (req, _res, next) => {
+		console.log(`${text} `, req.body);
+		next();
+	};
+}
+
+app.use(loggerMiddleware('before json'));
 app.use(express.json());
+app.use(loggerMiddleware('after json'));
+
+app.use(express.static('./templates'));
+// inclus le dossier public pour que le css s'applique
+app.use(express.static('./public'));
 
 app.use('/', router);
 
